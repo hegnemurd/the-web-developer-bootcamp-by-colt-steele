a7cf0123777dcda7a2e65d1e173685ecfd6ca963
hegnemurd <hegne.murd@gmail.com>
2020-09-19 20:38:09 +0100

    Section 10

---
+++ section-10-projects/js-age-calculator-exercise/js-age-calculator-exercise.js
@@ -0,0 +1,4 @@
+var ageYear = prompt("How old are you?");
+var ageDays = ageYear * 365.25;
+
+alert(ageYear + " year(s) is roughly " + ageDays + " days.");
---
+++ section-10-projects/js-stalker-exercise/js-stalker-exercise.js
@@ -0,0 +1,7 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+
+var fullName = userFirstName + " " + userLastName;
+console.log("Your full name is " + fullName + ".");
+console.log("You are " + userAge + " year(s) old.");
