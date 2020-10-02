71355b7f47ad72ac8cc8244826dbbed3962f3a63
hegnemurd <hegne.murd@gmail.com>
2020-10-02 21:00:19 +0100

    Section 14

---
+++ section-14-projects/javascript-nested-objects-and-arrays/javascript-nested-objects-and-arrays.js
@@ -0,0 +1,37 @@
+var posts = [
+  {
+    title: "Programming Popularity Around The World",
+    author: "Jane Doe",
+    comments: [
+      "Interesting.",
+      "I don't agree with the 3rd point.",
+      "Excellent article! Very enlightening.",
+    ],
+  },
+  {
+    title: "How Computers Changed The World",
+    author: "John Doe",
+    comments: [
+      "I wonder how it will change the future.",
+      "What happened if computers weren't invented.",
+      "I can't imagine a life without it.",
+    ],
+  },
+];
+
+console.log(
+  "In this example I nested some objects in an array called 'var posts' and in those objects I put some values and arrays."
+);
+console.log("Type in this bit of code and press enter:");
+console.log(
+  'var posts = [ { title: "Programming Popularity Around The World", author: "Jane Doe, comments: [ "Interesting.", "I do not agree with the 3rd point.", "Excellent article! Very enlightening.", ], }, { title: "How Computers Changed The World", author: "John Doe", comments: [ "I wonder how it will change the future.", "What happened if computers were not invented.", "I cannot imagine a life without it.", ], }, ];'
+);
+console.log(
+  "If you would like to access the object fully you can type in 'posts[0]'."
+);
+console.log(
+  "If you would like to access the the first objects first value 'object.[0].title' (syntax: object[0].firstValueName)."
+);
+console.log(
+  "If you would like to access the second comment in the second object type in 'posts[1].comments[1]'."
+);
---
+++ section-14-projects/javascript-objects-movie-database-exercise/javascript-objects-movie-database-exercise.js
@@ -0,0 +1,17 @@
+console.log(
+  "In this example we are creating an array with movies (with title, hasWatched and rating props) and then we will loop over the array and print out each object."
+);
+console.log("Type in the code underneath:");
+console.log(
+  'var movies = [ { title: "Frozen", hasWatched: false, rating: 4.5, }, { title: "Sherklock Holmes", hasWatched: true, rating: 5, }, { title: "Mission Impossible", hasWatched: false, rating: 4, }, { title: "The Lovely Bones", hasWatched: true, rating: 3.5, } ];'
+);
+console.log(
+  "Now that we have the objects created, we need to loop over them and make sentences to see if you have watched them or not."
+);
+console.log("Type in the following code:");
+console.log(
+  'movies.forEach(function(movie){ var result = "You have "; if(movie.hasWatched){ result += "watched "; } else { result += "not seen "; } result += """ + movie.title + "" - "; result += movie.rating + " stars." console.log(result); })'
+);
+console.log(
+  "To make it more readable or easier, we can move some of the code into a function instead."
+);
