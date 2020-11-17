869c0a4a5d3706fd7389340000fca020d6f1956b
hegnemurd <hegne.murd@gmail.com>
2020-11-17 19:50:30 +0000

    V2 Section 22

---
+++ v2-section-22-projects/array-methods/array-methods.js
@@ -0,0 +1,81 @@
+// ********** FOREACH **********
+
+// const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+// nums.forEach(function (n) {
+//   console.log(n * n);
+// });
+
+// nums.forEach(function (el) {
+//   if (el % 2 === 0) {
+//     console.log(el);
+//   }
+// });
+
+// const movies = [
+//   {
+//     title: "Frozen",
+//     score: 99,
+//   },
+//   {
+//     title: "Toy Story 4",
+//     score: 97,
+//   },
+//   {
+//     title: "Madagascar",
+//     score: 94,
+//   },
+//   {
+//     title: "Wreck-It Ralph",
+//     score: 100,
+//   },
+// ];
+
+// movies.forEach(function (movie) {
+//   console.log(`${movie.title} - ${movie.score}/100`);
+// });
+
+// ********** MAP **********
+
+// const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+// nums.map(function (num) {
+//   return num * 2;
+// });
+
+// const titles = movies.map(function (movie) {
+//     return movie.title
+// });
+
+// ********** OUTPUT TO CONSOLE **********
+
+console.log("********** FOREACH **********");
+
+console.log("const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];");
+console.log("EXAMPLE 1:");
+console.log("nums.forEach(function (n) { console.log(n * n); });");
+console.log("EXAMPLE 2:");
+console.log(
+  "nums.forEach(function (el) { if (el % 2 === 0) { console.log(el); } });"
+);
+console.log("EXAMPLE 3:");
+console.log(
+  "const movies = [ { title: 'Frozen', score: 99, }, { title: 'Toy Story 4', score: 97, }, { title: 'Madagascar', score: 94, }, { title: 'Wreck-It Ralph', score: 100, }, ];"
+);
+console.log(
+  "movies.forEach(function (movie) { console.log(`${movie.title} - ${movie.score}/100`); });"
+);
+
+console.log("********** MAP **********");
+
+console.log("EXAMPLE 1:");
+console.log("const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];");
+console.log("nums.map(function (num) { return num * 2; });");
+console.log("EXAMPLE 2:");
+console.log(
+  "const movies = [ { title: 'Frozen', score: 99, }, { title: 'Toy Story 4', score: 97, }, { title: 'Madagascar', score: 94, }, { title: 'Wreck-It Ralph', score: 100, }, ];"
+);
+console.log(
+  "const titles = movies.map(function (movie) { return movie.title });"
+);
+console.log("Type in: titles");
