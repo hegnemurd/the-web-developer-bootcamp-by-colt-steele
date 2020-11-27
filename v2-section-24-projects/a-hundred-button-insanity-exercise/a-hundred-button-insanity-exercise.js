1633c8de5cb546e4ff034be1b79d0ebb7cef406f
hegnemurd <hegne.murd@gmail.com>
2020-11-27 21:17:22 +0000

    V2 Section 24

---
+++ v2-section-24-projects/a-hundred-button-insanity-exercise/a-hundred-button-insanity-exercise.js
@@ -0,0 +1,4 @@
+console.log("Type this code in and see what happens:");
+console.log(
+  'let container = document.querySelector("#container"); for (let i = 0; i<100; i++){ let buttons = document.createElement("button"); buttons.innerText = "Hey!"; container.appendChild(buttons); }'
+);
---
+++ v2-section-24-projects/classlist-practice/classlist-practice.js
@@ -0,0 +1,4 @@
+console.log("Type in this code and see what happens:");
+console.log(
+  'let li = document.querySelectorAll("li"); for (let i = 0; i < li.length; i++) { li[i].classList.toggle("highlight"); }'
+);
