849d64d62515808ae2841527ff52454226cf7e82
hegnemurd <hegne.murd@gmail.com>
2021-01-19 21:59:53 +0000

    V2 Section 31

---
+++ v2-section-31-projects/terminal/boilerplate.js
@@ -0,0 +1,26 @@
+// const fs = require("fs");
+// const folderName = process.argv[2] || "Project";
+
+// fs.mkdir("Apples", { recursive: true }, (err) => {
+//   console.log("I'm in the callback");
+//   if (err) throw err;
+// });
+
+// try {
+//   fs.mkdirSync(folderName);
+//   fs.writeFileSync(`${folderName}/index.html`, "");
+//   fs.writeFileSync(`${folderName}/app.js`, "");
+//   fs.writeFileSync(`${folderName}/styles.css`, "");
+// } catch (e) {
+//   console.log("Something went wrong");
+//   console.log(e);
+// }
+
+console.log("I made this file using the terminal.");
+console.log("I used this file to create a new folder using the terminal.");
+console.log(
+  "I used this file to create a new folder with 3 new empty files using terminal."
+);
+console.log(
+  "By using terminal, I mean typing commands into the terminal to do all of these things."
+);
