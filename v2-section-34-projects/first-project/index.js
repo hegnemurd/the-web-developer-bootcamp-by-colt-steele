19507ddb1323d94b491b06c42fac5674559c0841
hegnemurd <hegne.murd@gmail.com>
2022-07-24 13:22:34 +0100

    V2 Section 34

--- v2-section-34-projects/first-project/index.js
+++ v2-section-34-projects/first-project/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const redditData = require("./data.json");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/cats", (req, res) => {
+  const cats = ["Blue", "Rocket", "Monty", "Stephanie", "Winston"];
+  res.render("cats", { cats });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  const data = redditData[subreddit];
+  if (data) {
+    res.render("subreddit", { ...data });
+  } else {
+    res.render("notfound", { subreddit });
+  }
 });
 
 app.get("/rand", (req, res) => {
